0900431839cc1a74164b5bcfb52cfbacccec06d7
manwich <johnrichpe@gmail.com>
2025-05-15 22:47:18 +0800

    Set database connection error handling in connection.php for testing

--- database/connection.php
+++ database/connection.php
@@ -4,9 +4,7 @@ $db_username = "root";
 $db_password = "";
 $db_database = "acadive";
 $conn = mysqli_connect($db_server, $db_username, $db_password, $db_database);
-if ($conn) {
-    echo "Connected";
-} else {
-    echo "Not connected";
+if (!$conn) {
+    echo "Not connected to database";
 }
 ?>
