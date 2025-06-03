2e3f2ccabcb244b427d2244c4113d1f21b2d11ce
manwich <johnrichpe@gmail.com>
2025-06-03 11:20:35 +0800

    Update database connection credentials and enhance login error handling

--- process/login.php
+++ process/login.php
@@ -15,34 +15,31 @@ if (mysqli_num_rows($result) > 0) {
         $_SESSION["error"] = "Your account is locked!";
         header("Location: ../login.php");
         exit;
-    }
-
-    if ($password == $row["password"]) {
+    }    if ($password == $row["password"]) {
         $_SESSION["username"] = $username;
         $_SESSION["user_id"] = $row["id"];
         header("Location: ../index.php");
         exit;
     }
+
     $query = "UPDATE users SET tries = tries + 1 WHERE username='$username'";
     mysqli_query($conn, $query);
-    
-    $new_tries = $row["tries"] + 1;
 
-    if ($new_tries >= 3) {
-        $query = "UPDATE users SET tries = 0, locked = 'Y' WHERE username='$username'";
+    if (++$row["tries"] >= 3) {
+        $query = "UPDATE users SET tries = 0 WHERE username='$username'";
+        mysqli_query($conn, $query);
+        $query = "UPDATE users SET locked = 'Y' WHERE username='$username'";
         mysqli_query($conn, $query);
         $_SESSION["error"] = "Your account is locked!";
         header("Location: ../login.php");
-        exit;
     } else {
-        $remaining_tries = 3 - $new_tries;
+        $remaining_tries = 3 - $row["tries"];
         if ($remaining_tries == 1) {
             $_SESSION["error"] = "Incorrect password! You have $remaining_tries try left.";
         } else {
             $_SESSION["error"] = "Incorrect password! You have $remaining_tries tries left.";
         }
         header("Location: ../login.php");
-        exit;
     }
 } else {
     $_SESSION["error"] = "Username not found!";
