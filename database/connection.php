365923ff67d5dfabc341da10a50f0758585893ba
Khalexyz <karlalexisagustin0974@gmail.com>
2025-06-03 10:40:13 +0800

    Update connection.php

--- database/connection.php
+++ database/connection.php
@@ -1,9 +1,9 @@
 <?php
 $db_server = "localhost";
 $db_username = "root";
-$db_password = "";
+$db_password = "1234";
 $db_database = "acadive";
-$conn = mysqli_connect($db_server, $db_username, $db_password, $db_database);
+$conn = mysqli_connect($db_server,$db_username,$db_password,$db_database);
 if (!$conn) {
     echo "Not connected to database";
 }
